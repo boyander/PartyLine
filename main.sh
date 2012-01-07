574dcd9722179250b258d2477b4e26047f1967ae
Marc Pomar Torres <boyander@gmail.com>
2012-01-07 18:47:03 +0100

    - Final Codec validation (CVSD Headerless at 16 kbps) - Streaming via multicast done!
    Signed-off-by: Marc Pomar Torres <boyander@gmail.com>

--- main.sh
+++ main.sh
@@ -4,40 +4,72 @@
 # Projecte Final
 
 #MacPorts binary path (For MarcP Macbook coniguration)
-PATH=$PATH:/opt/local/bin:/opt/local/sbin
+PATH=/opt/local/bin:/opt/local/sbin:/usr/bin:/bin:/usr/sbin:/sbin:/usr/local/bin:/usr/X11/bin
 export PATH
 
 #Client Settings
 port=8888
+destination=224.1.0.1
+interface="en1"
 protocol="_partyline._udp"
 domain="local"
 instance="sat-partyline"
-codec="vox"
+codec="cvsd"
 samplerate="8k"
 buffSizeBytes="1024"
 TXT_RECORD="codec=${codec}"
+recfifo="/tmp/recSat.tmp"
+playfifo="/tmp/playSat.tmp"
+
+
+#Origin IP ADDRESS
+function setIP_ADDR()
+{
+	host_type=$( uname -s )
+	if [ $host_type == "Linux" ]; then
+		ip_origin=$(ifconfig $interface | grep 'inet addr:' | cut -d: -f2 | awk '{print $1}')
+	elif [ $host_type == "Darwin" ]; then
+		ip_origin=$(ipconfig getifaddr $interface)
+	fi
+	echo "Origin IP is $ip_origin"
+}
 
 #Start Service Routine
-function startService()
+function startRecordingService()
 {
 	#Register local Service with Apple Bonjour
 	mDNS -R $instance $protocol $domain $port $TXT_RECORD > dnsLog.txt &
 	dnsPid=$!
 	echo "Service Registered (mDNS pid -> ${dnsPid})"
 	
-	#Configure SOX audio options
-	recOptions="-V -t $codec -c 1 -r $samplerate --buffer $buffSizeBytes"
-	playOptions="-t $codec -c 1 -r $samplerate --buffer $buffSizeBytes"
-	
 	#Create recording fifo
-	recfifo="/tmp/recSat.tmp"
+	rm -f $recfifo
+
 	mkfifo $recfifo
+	
+	#Configure SOX audio options
+	recOptions="-V -t $codec -c 1 -r $samplerate --buffer $buffSizeBytes "
 	
 	#Start Recording
 	rec $recOptions $recfifo & 
 	recPid=$!
 	
-	play $playOptions $recfifo &
+	#Start Sending multicast stream
+	socat -u PIPE:$recfifo UDP4-DATAGRAM:$destination:$port &
+	sendPid=$!
+}
+
+function startPlayIncoming(){
+	#Create playfifo
+	rm -f $playfifo
+	mkfifo $playfifo
+	
+	#Read local network multicast
+	socat -u UDP-RECV:8888,ip-add-membership=224.1.0.1:$ip_origin PIPE:$playfifo &	 
+	rcvPid=$!
+	
+	playOptions="-V -t $codec -c 1 -r $samplerate --buffer $buffSizeBytes"
+	play $playOptions $playfifo &
 	playPid=$!
 }
 
@@ -47,26 +79,27 @@ function stopService()
 	echo "ByeBye, killing service and sox audio..."
 	
 	#Remove bonjour local service
-	echo "Killed bonjour service ${dnsPid}"
 	kill -9 $dnsPid
 	
 	#Kill sox audio
-	echo "Killed rec ${recPid}"
 	kill -9 $recPid
-	echo "Killed play ${recPid}"
 	kill -9 $playPid
-	#Remove fifo
-	rm -f $recfifo
+	
+	#Stop Sending packets tru network
+	kill -9 $sendPid
+	kill -9 $rcvPid
 	
 	#Kill script ;)
 	exit 0
 }
 
-
-
+#Set origin IP ADRESS
+setIP_ADDR
 
 #Start PartyLine Service
-startService
+startRecordingService
+startPlayIncoming
+
 echo "Service started -> Play[${playPid}], Rec[${recPid}]"
 
 #Stop Service on Exit
